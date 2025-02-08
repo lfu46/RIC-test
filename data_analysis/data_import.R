fad07f8239e33a57813a5f03d2099eef2124001b
Longping Fu <lpfu46@gatech.edu>
2025-02-08 12:22:29 -0500

    add xlsx file

--- data_analysis/data_import.R
+++ data_analysis/data_import.R
@@ -2,3 +2,9 @@
 packages_names <- c('tidyverse', 'readxl', 'writexl')
 lapply(packages_names, require, character.only = TRUE)
 
+#import result files
+RIC_01232025_result <- read_xlsx(
+  'data_source/raw_data/RIC_01232025.xlsx', 
+  col_names = TRUE,
+  .name_repair = 'universal'
+)
